ca06a0e968bdc7d0079d21fb891cb9f93cdb868e
mocakosan@naver.com <mocakosan@naver.com>
2022-10-14 16:32:56 +0900

    first commit

---
+++ component/TodoList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {StyleSheet, ScrollView, Text} from 'react-native';
+import TodoListItem from './TodoListItem';
+
+const TodoList = ({todos}) => {
+  return (
+    <ScrollView contentContainerStyle={styles.listContainer}>
+      {todos.map(todo => (
+        <TodoListItem key={todo.id} {...todo} />
+      ))}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  listContainer: {
+    alignItems: 'center',
+  },
+});
+
+export default TodoList;
---
+++ component/TodoListItem.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+const TodoListItem = ({textValue, id, checked}) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <View style={styles.circle} />
+      </TouchableOpacity>
+      <Text style={styles.text}>TodoList items will be shown here</Text>
+      <TouchableOpacity style={styles.buttonContainer}>
+        <View style={styles.circle} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  text: {
+    flex: 5,
+    fontWeight: '500',
+    fontSize: 18,
+    marginVertical: 20,
+    width: 100,
+  },
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderColor: 'blue',
+    borderWidth: 2,
+    marginRight: 20,
+    marginLeft: 20,
+  },
+});
+
+export default TodoListItem;
